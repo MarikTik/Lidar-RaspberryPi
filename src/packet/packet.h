7aac5fb85c8d565869b2ce5e09e707eac02d2462
MarikTik <mtik.philosopher@gmail.com>
2024-02-11 19:51:14 -0800

    renaming 'Measurement' to 'Point'
    it makes more sense for a point to be represented by distance and an angle

--- src/packet/packet.h
+++ src/packet/packet.h
@@ -3,23 +3,23 @@
 
 
 namespace lidar{
-     static constexpr std::size_t measurements_per_packet_default = 12;
+     static constexpr std::size_t points_per_packet_default = 12;
      static constexpr std::uint8_t frame_header_signature = 0x54;
 
      #pragma pack(push, 1)  
 
-     struct Measurement{
+     struct Point{
           std::uint16_t distance;
           std::uint16_t angle;
      };
 
-     template<std::size_t measurements_per_frame = measurements_per_packet_default>
+     template<std::size_t points_per_frame = points_per_packet_default>
      struct Packet{
           std::uint8_t header;
           std::uint8_t verLen;
           std::uint16_t speed;
           std::uint16_t startAngle;
-          lidar::Measurement measurments[measurements_per_frame];
+          lidar::Point measurments[points_per_frame];
           std::uint16_t endAngle;
           std::uint16_t timestamp;
           std::uint8_t crc8; // This field is filled last, after CRC calculation
