7dd3557cd816d0c58bc098bf83130da8515c2c8d
Mark <mtik.philosopher@gmail.com>
2024-02-19 22:22:43 -0800

    changing uid() method to be constexpr

--- src/packet/packet_uid.h
+++ src/packet/packet_uid.h
@@ -7,7 +7,7 @@ namespace lidar::transmission {
         static_assert(std::is_invocable_r_v<std::uint16_t, decltype(&packet_type::uid), packet_type>, 
                       "packet_type must implement a uid() method returning std::uint16_t");
 
-        auto uid() -> decltype(static_cast<packet_type*>(this)->uid()) {
+        constexpr auto uid() -> decltype(static_cast<packet_type*>(this)->uid()) {
             return static_cast<packet_type*>(this)->uid();
         }
     };
