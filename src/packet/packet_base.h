6c0bb7b9bca559e5677ce41d64dfa0d7c687bbd5
Mark <mtik.philosopher@gmail.com>
2024-02-20 12:22:40 -0800

    editing the message of static aseertion in PacketBase::header

--- src/packet/packet_base.h
+++ src/packet/packet_base.h
@@ -2,14 +2,13 @@
 #include <type_traits>
 
 namespace lidar::transmission {
+    
     template<typename packet_type>
-    struct PacketBase {
-        //packet_type::uid has to be an integer type, i.e. uint8_t, uint16_t, ..., uint64_t and bigger types
-       
+    struct PacketBase {       
         static constexpr auto header() {
             static_assert(
                 std::is_integral_v<decltype(packet_type::header())>,   
-                "packet_type must implement a uid() method returning an integral type"
+                "packet_type must implement a `header` method and return an integral type"
             );
 
             return packet_type::header();
