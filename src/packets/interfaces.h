7711ec09c99d77f4208b94bc3229bbc672154df4
Mark <mtik.philosopher@gmail.com>
2024-02-20 14:49:34 -0800

    added stream interface to the list of interfaces.h

--- src/packets/interfaces.h
+++ src/packets/interfaces.h
@@ -3,5 +3,6 @@
 #include "interfaces/packet_interface.h"
 #include "interfaces/gpio.h"
 #include "interfaces/serial.h"
+#include "interfaces/stream.h"
 #include "interfaces/tcp.h"
 #include "interfaces/udp.h"
