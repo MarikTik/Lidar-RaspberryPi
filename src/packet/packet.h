eab3bee1f904cfbd2ab9a6651885d1fae9af3e32
Mark <mtik.philosopher@gmail.com>
2024-02-20 13:08:16 -0800

    renaming  function back to
    it was causing problems since  was also a member of class Packet

--- src/packet/packet.h
+++ src/packet/packet.h
@@ -1,13 +1,9 @@
 #pragma once
 #include <cstdint>
-#include <array>
-#include <iomanip>
-#include "packet_uid.h"
+#include "packet_base.h"
 
 namespace lidar::transmission {
      static constexpr std::size_t points_per_packet_default = 12; ///< Default number of points per packet
-     static constexpr std::uint8_t frame_header_signature = 0x54; ///< Signature for the frame header
-
 
      #pragma pack(push, 1)  
      /**
@@ -23,43 +19,27 @@ namespace lidar::transmission {
       * @struct Packet
       * @brief Structure for receiving packets from a Lidar.
       * 
-      * @tparam points_per_frame Number of points in a single frame. Defaults to `points_per_packet_default`.
+      * @tparam N Number of points in a single frame. Defaults to `points_per_packet_default`.
       */
-     template<std::size_t points_per_frame = points_per_packet_default>
-     struct Packet : public PacketUID<Packet<points_per_frame>> {
+     template<std::size_t N = points_per_packet_default>
+     struct Packet : public PacketBase<Packet<N>> {
           std::uint8_t header; ///< Packet header
           std::uint8_t ver_len; ///< Version and length
           std::uint16_t speed; ///< Rotation speed
           std::uint16_t start_angle; ///< Angle at the start of the scan
-          PointData points[points_per_frame]; ///< Array of Lidar points
+          PointData points[N]; ///< Array of Lidar points
           std::uint16_t end_angle; ///< Angle at the end of the scan
           std::uint16_t timestamp; ///< Timestamp of the packet
           std::uint8_t crc8; ///< CRC8 checksum, calculated last
 
+          
           static constexpr std::uint16_t uid(){
-               return 0b1100001000101100;
+               constexpr std::uint8_t header_signature = 0x54;  
+               constexpr std::uint8_t ver_len_signature = 0x2C;
+
+               return static_cast<uint16_t>(header_signature) << 8 | ver_len_signature; 
           }
-          template<std::size_t N>
-          friend std::ostream& operator<<(std::ostream& os, const Packet<N>& packet);
      };
      #pragma pack(pop)
-     
 
-     // template<std::size_t N>
-     // std::ostream& operator<<(std::ostream& os, const Packet<N>& packet) {
-     //      os << "Header: " << static_cast<int>(packet.header)
-     //         << "\nVersion-Length: " << static_cast<int>(packet.ver_len)
-     //         << "\nSpeed: " << packet.speed
-     //         << "\nStart Angle: " << packet.start_angle
-     //         << "\nEnd Angle: " << packet.end_angle
-     //         << "\nTimestamp: " << packet.timestamp
-     //         << "\nCRC: " << static_cast<int>(packet.crc8) << "\nPoints:\n";
-     //      for (size_t i = 0; i < N; ++i) {
-     //        os << "  [" << std::setw(2) << std::setfill('0') << i << "]: "
-     //           << "\nDistance: " << packet.points[i].distance
-     //           << "\nIntensity: " << packet.points[i].intensity << std::endl;
-     //      }
-     //      return os;
-     // }
-   
 } // namespace lidar
--- src/packet/packet_base.h
+++ src/packet/packet_base.h
@@ -2,17 +2,18 @@
 #include <type_traits>
 
 namespace lidar::transmission {
-    
+
     template<typename packet_type>
     struct PacketBase {       
-        static constexpr auto header() {
+        static constexpr auto uid() {
             static_assert(
-                std::is_integral_v<decltype(packet_type::header())>,   
-                "packet_type must implement a `header` method and return an integral type"
+                std::is_integral_v<decltype(packet_type::uid())>,   
+                "packet_type must implement a `uid` method and return an integral type"
             );
 
-            return packet_type::header();
+            return packet_type::uid();
         }
     };
 }
 
+//   return 0b1100001000101100;
