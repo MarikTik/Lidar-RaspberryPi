2f52f8279e32e0d246116b5de2601017788db568
Mark <mtik.philosopher@gmail.com>
2024-02-19 23:41:41 -0800

    changing the packet uid() method to be a named requirement
    i.e. it can return any type, as long as it's name is 'uid'

--- src/packet/packet_uid.h
+++ src/packet/packet_uid.h
@@ -4,7 +4,8 @@
 namespace lidar::transmission {
     template<typename packet_type>
     struct PacketUID {
-        static_assert(std::is_invocable_r_v<std::uint16_t, decltype(&packet_type::uid), packet_type>, 
+        //packet_type::uid has to be an integer type, i.e. uint8_t, uint16_t, ..., uint64_t and bigger types
+        static_assert(std::is_invocable_v<decltype(packet_type::uid)>,   
                       "packet_type must implement a uid() method returning std::uint16_t");
 
         static constexpr auto uid() -> decltype(packet_type::uid()) {
