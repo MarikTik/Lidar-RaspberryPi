6ce451c2188acdafa976085c8b602bcd50ce957a
Mark <mtik.philosopher@gmail.com>
2024-02-19 21:37:25 -0800

    adding option to read more then one with function read()

--- src/packet/packet_reader.h
+++ src/packet/packet_reader.h
@@ -2,37 +2,44 @@
 #include <cstdint>
 #include <type_traits>
 #include "packet_uid.h"
+#include <cstdint>
 
 namespace lidar::transmission{
      template<typename Packet>
      struct IPacketReader{
           static_assert(std::is_base_of_v<PacketUID<Packet>, Packet>, "Packet has to derive from PacketUID");
-          virtual Packet read() = 0;
+          virtual Packet read(std::size_t packets) = 0;
      };
 
 
      template<typename Packet>
-     struct SerialPacketReader : public IPacketReader<Packet>
+     class SerialPacketReader : public IPacketReader<Packet>
      {
-          SerialPacketReader(const char* port, uint32_t baud_rate);
-          Packet read() override;
+          public:
+               SerialPacketReader(const char* port, uint32_t baud_rate);
+               Packet read(std::size_t packets) override;
+
+
      };
 
      template<typename Packet>
-     struct GPIOPacketReader
+     class GPIOPacketReader
      {
           GpioPacketReader(uint8_t rx, uint32_t baud_rate);
-          Packet read() override; //not implemented
+          Packet read(std::size_t packets) override; //not implemented
      };
      
      template<typename Packet>
-     struct UdpPacketReader{
+     class UdpPacketReader{
           //not implemented
      };
 
      template<typename Packet>
-     struct TcpPacketReader{
+     class TcpPacketReader{
           //not implemented
      };
      
+     
 }
+
+#include "packet_reader.tpp"
