d651380a23546697e7b7b964fb03f75637144b18
Mark <mtik.philosopher@gmail.com>
2024-02-20 20:34:19 -0800

    adding factory method  for every packet

--- src/packets/types/packet_base.h
+++ src/packets/types/packet_base.h
@@ -1,19 +1,40 @@
 #pragma once
 #include <type_traits>
+#include <cstdint>
+#include <cstring>
 
 namespace lidar::transmission {
 
-    template<typename packet_type>
+    template<typename TPacket>
     struct PacketBase {       
         static constexpr auto uid() {
             static_assert(
-                std::is_integral_v<decltype(packet_type::uid())>,   
+                std::is_integral_v<decltype(TPacket::uid())>,   
                 "packet_type must implement a `uid` method and return an integral type"
             );
 
-            return packet_type::uid();
+            return TPacket::uid();
         }
+        template<typename Byte, std::size_t N>
+        static constexpr TPacket create(const Byte (&bytes)[N]){
+            static_assert(
+                std::is_same_v<Byte, uint8_t> || std::is_same_v<Byte, int8_t>,
+                "create only accepts 8-bit sequences, either uint8_t or int8_t."    
+            );
+            static_assert(
+                N == sizeof(TPacket),
+                "The size of the passed bytes array should exactly match the size of TPacket."
+            );
+            static_assert(
+                std::is_trivially_copyable_v<TPacket>,
+                "TPacket must be trivially copyable."
+            );
+            // no checks for endianess!
+            
+            TPacket packet;
+            std::memcpy(&packet, bytes, sizeof(TPacket)); 
+            return packet;
+        }   
+
     };
 }
-
-//   return 0b1100001000101100;
