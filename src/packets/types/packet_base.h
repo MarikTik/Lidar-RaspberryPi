fd9ddd8cad381196ba47da1acb6fc54b9dc1265f
Mark <mtik.philosopher@gmail.com>
2024-02-20 20:39:30 -0800

    moved implementation of PacketBase to packet_base.tpp file

--- src/packets/types/packet_base.h
+++ src/packets/types/packet_base.h
@@ -7,34 +7,10 @@ namespace lidar::transmission {
 
     template<typename TPacket>
     struct PacketBase {       
-        static constexpr auto uid() {
-            static_assert(
-                std::is_integral_v<decltype(TPacket::uid())>,   
-                "packet_type must implement a `uid` method and return an integral type"
-            );
-
-            return TPacket::uid();
-        }
+        static constexpr auto uid();
         template<typename Byte, std::size_t N>
-        static constexpr TPacket create(const Byte (&bytes)[N]){
-            static_assert(
-                std::is_same_v<Byte, uint8_t> || std::is_same_v<Byte, int8_t>,
-                "create only accepts 8-bit sequences, either uint8_t or int8_t."    
-            );
-            static_assert(
-                N == sizeof(TPacket),
-                "The size of the passed bytes array should exactly match the size of TPacket."
-            );
-            static_assert(
-                std::is_trivially_copyable_v<TPacket>,
-                "TPacket must be trivially copyable."
-            );
-            // no checks for endianess!
-            
-            TPacket packet;
-            std::memcpy(&packet, bytes, sizeof(TPacket)); 
-            return packet;
-        }   
-
+        static constexpr TPacket create(const Byte (&bytes)[N]);
     };
 }
+
+#include "packet_base.tpp"
---
+++ src/packets/types/packet_base.tpp
@@ -0,0 +1,38 @@
+#include "packet_base.h"
+
+namespace lidar::transmission{
+     template <typename TPacket>
+     constexpr auto PacketBase<TPacket>::uid()
+     {
+          static_assert(
+              std::is_integral_v<decltype(TPacket::uid())>,   
+              "packet_type must implement a `uid` method and return an integral type"
+          );   
+          return TPacket::uid();
+     }
+     
+
+     template<typename TPacket>
+     template<typename Byte, std::size_t N>
+     inline constexpr TPacket PacketBase<TPacket>::create(const Byte(& bytes)[N])
+     {
+          static_assert(
+              std::is_same_v<Byte, uint8_t> || std::is_same_v<Byte, int8_t>,
+              "create only accepts 8-bit sequences, either uint8_t or int8_t."    
+          );
+          static_assert(
+              N == sizeof(TPacket),
+              "The size of the passed bytes array should exactly match the size of TPacket."
+          );
+          static_assert(
+              std::is_trivially_copyable_v<TPacket>,
+              "TPacket must be trivially copyable."
+          );
+          // no checks for endianess!
+
+          TPacket packet;
+          std::memcpy(&packet, bytes, sizeof(TPacket)); 
+          return packet;
+     }
+
+}
