0c8b2352efb0489c5b45ca65121894c385bff2c4
MarikTik <mtik.philosopher@gmail.com>
2024-03-19 16:55:46 -0700

    moved implementation of Point to the header file and changed x,y fields to be inline functions

---
+++ src/packets/data/point.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <cstdint>
+#include <cmath>
+
+namespace lidar::data{
+     using namespace std;
+
+     struct Point{ 
+          float angle;
+          uint16_t distance;
+          uint8_t intensity;
+          uint64_t time_stamp; 
+
+          inline float x();
+          inline float y();
+
+          Point() = default;
+          constexpr Point(float angle, uint16_t distance, uint8_t intensity, uint64_t stamp);
+          
+     };
+
+     inline float Point::x(){
+
+          return distance * cos(angle * M_PI / 180.0f);
+     }
+
+     inline float Point::y()
+     {
+          return distance * sin(angle * M_PI / 180.0f);
+     }
+
+     constexpr Point::Point(float angle, uint16_t distance, uint8_t intensity, uint64_t stamp)
+          : angle(angle), distance(distance), intensity(intensity), time_stamp(stamp)
+     {
+     }
+}
+
