b007c2c57e1a8c045c64deb8db557b10167f69fd
MarikTik <mtik.philosopher@gmail.com>
2024-03-19 23:41:39 -0700

    moved the implementation of serial_interface functions to serial_interface.tpp file and added buffer support

--- src/interfaces/serial_interface.hpp
+++ src/interfaces/serial_interface.hpp
@@ -30,69 +30,18 @@ public:
      bool is_open() const;
      bool open(const std::string& port);
      void close();
-
+     void set_buffer_size(std::size_t size);
+     
 private:
      boost::asio::serial_port _serial_port;
      connection_settings _connection_settings;
 
+     std::vector<uint8_t> _buffer;
      mutable std::shared_mutex _mutex;
      mutable std::condition_variable _cv;
-
 };
 
-
-SerialInterface::SerialInterface(asio::io_context &io_context)
-     : AsyncInterface<SerialInterface>(io_context), _serial_port(io_context)
-{
-}
-
-bool SerialInterface::open(const std::string& port)
-{
-     system::error_code ec;
-     if (_serial_port.is_open()){
-          std::cerr << "Error: Lidar is already opened. Closing the old one and opening " << port << "\n"; 
-          close();
-     }
-     _serial_port.open(port, ec);
-     if (ec) 
-     {
-          std::cerr << "Error from open: " << ec.message() << std::endl;
-          return false;
-     }
-    
-     _serial_port.set_option(boost::asio::serial_port_base::baud_rate(_connection_settings.baud_rate), ec);
-     _serial_port.set_option(boost::asio::serial_port_base::character_size(_connection_settings.character_size), ec);
-     _serial_port.set_option(boost::asio::serial_port_base::stop_bits(_connection_settings.stop_bits), ec);
-     _serial_port.set_option(boost::asio::serial_port_base::parity(_connection_settings.parity), ec);
-     _serial_port.set_option(boost::asio::serial_port_base::flow_control(_connection_settings.flow_control), ec);
-
-     if (ec) 
-     {
-          std::cerr << "Error from setting options: " << ec.message() << std::endl;
-          close();
-          return false;
-     }
-     return true;
-}
-
-void SerialInterface::close()
-{
-     if (_serial_port.is_open())
-          _serial_port.close();
-}
-
-bool SerialInterface::is_open() const
-{
-     return _serial_port.is_open();
-}
-template<typename Data> std::vector<Data> SerialInterface::async_read() const
-{
-}
-
-void SerialInterface::async_scan()
-{
-}
-
+#include "serial_interface.tpp"
  
 
 
