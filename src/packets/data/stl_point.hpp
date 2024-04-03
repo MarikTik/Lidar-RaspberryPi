3335946fddbc37b39f023e5516b1a2fc35261cfe
MarikTik <mtik.philosopher@gmail.com>
2024-04-02 21:54:41 -0700

    fixed unused template paramter for stl point constructor

--- src/packets/data/stl_point.hpp
+++ src/packets/data/stl_point.hpp
@@ -1,44 +1,51 @@
 #pragma once
 #include <cstdint>
 #include <cmath>
+#include "../types/stl_packet.hpp"
 
 namespace lidar::data::stl{
+     using namespace transmission::stl;
      struct Point{ 
-          float angle; //degrees
+          float angle; //radians (0-2π)
           uint16_t distance; //millimeters
           uint8_t intensity; // 0-255
 
           inline float x();
           inline float y();
-
-          Point() = default;
+          
+          // angle in range 0-36000, distance in mm, intensity 0-255
           constexpr Point(float angle, uint16_t distance, uint8_t intensity);
 
+          Point() = default;
           struct SuppliedData{
-               uint16_t angular_speed; //degrees per second
+               float angular_speed; //radians per second
                uint16_t timestamp; //milliseconds
-               SuppliedData() = default;
-               constexpr SuppliedData(uint16_t angular_speed, uint16_t timestamp);
+
+               template<size_t N>
+               constexpr SuppliedData(const Packet<N>& packet);
           };
      };
 
      inline float Point::x(){
-          return distance * cos(angle * M_PI / 180.0f);
+          return distance * cos(angle);
      }
 
      inline float Point::y()
      {
-          return distance * sin(angle * M_PI / 180.0f);
+          return distance * sin(angle);
      }
      
-     constexpr Point::Point(float angle, uint16_t distance, uint8_t intensity)
-          : angle(angle), distance(distance), intensity(intensity)
+     inline constexpr Point::Point(float angle, uint16_t distance, uint8_t intensity)
+          : angle(angle * (M_PI / 18000.f)), distance(distance), intensity(intensity)
+     {
+     }
+   
+     template <size_t N>
+     inline constexpr Point::SuppliedData::SuppliedData(const Packet<N> &packet)
+          : angular_speed(packet.speed / (M_PI / 18000.f)), timestamp(packet.timestamp)
      {
      }
 
-     constexpr Point::SuppliedData::SuppliedData(uint16_t angular_speed, uint16_t timestamp)
-          : angular_speed(angular_speed), timestamp(timestamp)
-     {
-     }
+   
+
 }
-
